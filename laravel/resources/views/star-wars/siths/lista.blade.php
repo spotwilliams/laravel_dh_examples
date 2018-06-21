f2c76e3e723b0a66e80ed76d2b042f9c07a55b90
Richard V. 121 <ricardo@121ecommerce.com>
2018-06-21 13:11:37 -0300

    Mejora de vistas blade

--- laravel/resources/views/star-wars/siths/lista.blade.php
+++ laravel/resources/views/star-wars/siths/lista.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.default')
 
 @section('contenido')
-    <ul>
+    <div class="my-auto">
+        <h1 class="mb-0">Todos los
+            <span class="text-primary">Sith</span>
+        </h1>
+        <ul>
 
-        @foreach($siths as $sith)
-            <li>{{$sith}}</li>
-        @endforeach
-    </ul>
+            @foreach($siths as $sith)
+                <li>{{$sith}}</li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
