429ae0060a0385e053049092e593fed28da9ed31
Richard V. 121 <ricardo@121ecommerce.com>
2018-07-05 00:55:03 -0300

    Deleting databases and seeds

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -21,7 +21,3 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
-
-$factory->define(\App\Models\Director::class, function (Faker $faker) {
-    return ['name' => $faker->name];
-});
